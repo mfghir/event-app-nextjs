724774c4c20ca1bc069dc08a5236b386e1861f4f
miss-f-g <fatemeghafari98@gmail.com>
2023-02-27 12:12:06 +0330

    7.add layout

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,9 +1,28 @@
-import React from 'react'
+import EventContent from "@/components/event-detail/event-content";
+import EventLogistics from "@/components/event-detail/event-logistics";
+import EventSummary from "@/components/event-detail/event-summary";
+import { getEventById } from "@/data";
+import { useRouter } from "next/router";
+import { Fragment } from "react";
 
 const EventDetailPage = () => {
-  return (
-    <div>Event Detail Page</div>
-  )
-}
+  const router = useRouter();
+  const eventId = router.query.eventId;
 
-export default EventDetailPage
+  const event = getEventById(eventId);
+  if (!event) {
+    return <p>no event found</p>;
+  }
+
+  return (
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics event={event}  />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
+  );
+};
+
+export default EventDetailPage;
